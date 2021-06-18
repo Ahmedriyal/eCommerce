f02a7e6365ce4c81c18f1a13808750dd44852132
AhmedRiyal <ahmedriyal@gmail.com>
2021-06-18 23:15:14 +0600

    Football Apparel website using django

---
+++ eCommerce/jerseyshop/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'html/home.html')
